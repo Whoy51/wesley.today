ae09eaf2fdf6e72c64bd73a0ecf87f959e401031
Wesley Hoy <113642322+Whoy51@users.noreply.github.com>
2023-01-24 14:26:09 -0500

    Added ability to make posts

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import flask
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+myList = [[1, "My Title", "This is a blog post"], [2, "My Title 2", "This is a blog post 2"]]
 
 @app.route('/')
 def index():
@@ -16,6 +18,16 @@ def about():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+
+@app.route('/post/<post>')
+def post(post):
+    if myList.__len__() >= int(post):
+        title = myList[int(post)][1];
+        content = myList[int(post)][2];
+        return render_template('post.html', title=title, content=content)
+    else:
+        return render_template('post.html')
 
 
 if __name__ == '__main__':
